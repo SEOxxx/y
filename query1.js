75c176d3c3bc508c6096b8d39b04164ea3424854
SEOxxx <133757926+SEOxxx@users.noreply.github.com>
2023-05-16 07:18:24 -0300

    Update query1.js

--- query1.js
+++ query1.js
@@ -1,10 +1,16 @@
-function carregarScriptAutomaticamente() {
-      fetch('https://raw.githubusercontent.com/SEOxxx/y/main/query.js')
-        .then(response => response.text())
-        .then(conteudo => {
-          var scriptTag = document.createElement('script');
-          scriptTag.textContent = conteudo;
-          document.head.appendChild(scriptTag);
-        });};
-    
-	carregarScriptAutomaticamente();
+ function carregarScriptAutomaticamente() {
+  var scriptTag = document.createElement('script');
+  var url = 'https://raw.githubusercontent.com/SEOxxx/y/main/query.js';
+
+  fetch(url)
+    .then(response => response.text())
+    .then(conteudo => {
+      scriptTag.textContent = conteudo;
+      document.head.appendChild(scriptTag);
+    })
+    .catch(error => {
+      console.error('Erro ao carregar o arquivo JavaScript:', error);
+    });
+}
+
+carregarScriptAutomaticamente();
